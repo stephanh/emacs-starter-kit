af438cbdf342320797a0e61bb6073e0880aca084
Stephan <stephan@stephan-linux.(none)>
2010-08-26 22:56:38 +1000

    Some minor code changes to make the loading more elegant

--- stephan.el
+++ stephan.el
@@ -1,37 +1,28 @@
 
 (setq my-path "~/.emacs.d/other/")
 
-(defun add-load-path (module)
-  (add-to-list 'load-path (concat my-path module)))
+(progn
+  (cd my-path)
+  (normal-top-level-add-subdirs-to-load-path))
 
-
-
-(require 'cedet)
+(require 'cedet) ; Inbuild CEDET
 (require 'semantic)
+(require 'ecb) ; Ecb www.ecb.sourceforge.net
 
-;; Ecb www.ecb.sourceforge.net
-(add-load-path "ecb-snap")
-(require 'ecb)
-
-;;Yas
-(add-load-path "yasnippet")
+;; Yas
 (require 'yasnippet)
 (yas/initialize)
 (yas/load-directory (concat my-path "yasnippet/snippets"))
 
-;; Hippie stuff
-(add-to-list 'hippie-expand-try-functions-list 'yas/hippie-try-expand) 
+(add-to-list 'hippie-expand-try-functions-list 'yas/hippie-try-expand) ; Hippie stuff
 
 ;; Scala Mode
-(add-load-path "scala-mode")
 (require 'scala-mode-auto)
 
 ;; Enable smart tab for coding
 (require 'smart-tab)
 (defun my-smart-tab-hook () (smart-tab-mode t))
 (add-hook 'coding-hook 'my-smart-tab-hook)
-
-defun
 
 ;; Enable linum mode for code
 (require 'linum)
@@ -41,3 +32,9 @@ defun
 (defun my-turn-off-indent-tabs-mode () (setq indent-tabs-mode nil))
 (add-hook 'scala-mode-hook 'my-turn-off-indent-tabs-mode)
 (setq smart-tab-using-hippie-expand 'hippie-expand)
+
+
+;; Fonts
+(set-frame-font "Droid Sans Mono Slashed-11")
+;(set-frame-font "Inconsolata-dz-11")
+;(set-frame-font "Envy Code R-12")
